c8b1cabb7bee6fa2ee406ef5a0f2a65c2da3ffe0
Devpas1998 <106461487+Devpas1998@users.noreply.github.com>
2022-08-27 04:29:49 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ var port = process.env.PORT || 8080;
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+var originWhitelist = ['http://localhost:8100', 'http://localhost', 'capacitor://localhost:8100', 'cordova://http://localhost:8100']
+
 function parseEnvList(env) {
-  if (!env) {
-    return [];
-  }
-  return env.split(',');
+    if (!env) {
+        return [];
+    }
+    return env.split(',');
 }
 
 // Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
@@ -21,29 +22,29 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
-  originBlacklist: originBlacklist,
-  originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
-  checkRateLimit: checkRateLimit,
-  removeHeaders: [
-    'cookie',
-    'cookie2',
-    // Strip Heroku-specific headers
-    'x-request-start',
-    'x-request-id',
-    'via',
-    'connect-time',
-    'total-route-time',
-    // Other Heroku added debug headers
-    // 'x-forwarded-for',
-    // 'x-forwarded-proto',
-    // 'x-forwarded-port',
-  ],
-  redirectSameOrigin: true,
-  httpProxyOptions: {
-    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
-    xfwd: false,
-  },
+    originBlacklist: originBlacklist,
+    originWhitelist: originWhitelist,
+    requireHeader: ['origin', 'x-requested-with'],
+    checkRateLimit: checkRateLimit,
+    removeHeaders: [
+        'cookie',
+        'cookie2',
+        // Strip Heroku-specific headers
+        'x-request-start',
+        'x-request-id',
+        'via',
+        'connect-time',
+        'total-route-time',
+        // Other Heroku added debug headers
+        // 'x-forwarded-for',
+        // 'x-forwarded-proto',
+        // 'x-forwarded-port',
+    ],
+    redirectSameOrigin: true,
+    httpProxyOptions: {
+        // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+        xfwd: false,
+    },
 }).listen(port, host, function() {
-  console.log('Running CORS Anywhere on ' + host + ':' + port);
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
